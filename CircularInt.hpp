b7fd20219af720ccfa41ea89be44fa9e58aaa983
Gal Turgeman <user154@gmail.com>
2018-04-29 20:07:55 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -24,43 +24,43 @@ public:
     ~CircularInt();
 
     // == !=  = //
-    CircularInt& operator = (CircularInt const &);
+    CircularInt& operator = (const CircularInt &);
     CircularInt& operator = (int num);
 
-    friend bool operator == (CircularInt const &,CircularInt const &);
-    friend bool operator == (int const,CircularInt const &);
-    friend bool operator == (CircularInt const &,int const);
+    friend bool operator == (const CircularInt &,const CircularInt &);
+    friend bool operator == (int const,const CircularInt &);
+    friend bool operator == (const CircularInt &,int const);
 
-    friend bool operator != (CircularInt const &,CircularInt const &);
+    friend bool operator != (const CircularInt &,const CircularInt &);
     friend bool operator != (int const,CircularInt const &);
-    friend bool operator != (CircularInt const &,int const);
+    friend bool operator != (const CircularInt &,int const);
     //<= >= > < //
     friend bool operator <= (CircularInt const &,int const);
-    friend bool operator <= (int const,CircularInt const &);
-    friend bool operator <= (CircularInt const &,CircularInt const &);
+    friend bool operator <= (int const,const CircularInt &);
+    friend bool operator <= (const CircularInt const &,const CircularInt &);
 
-    friend bool operator >= (CircularInt const &,int const);
+    friend bool operator >= (const CircularInt &,int const);
     friend bool operator >= (int const,CircularInt const &);
-    friend bool operator >= (CircularInt const &,CircularInt const &);
+    friend bool operator >= (const CircularInt &,const CircularInt &);
 
-    friend bool operator < (CircularInt const &,int const);
-    friend bool operator < (int const,CircularInt const &);
-    friend bool operator < (CircularInt const &,CircularInt const &);
+    friend bool operator < (const CircularInt  &,int const);
+    friend bool operator < (int const,const CircularInt &);
+    friend bool operator < (const CircularInt &,const CircularInt &);
 
-    friend bool operator > (CircularInt const &,int const);
-    friend bool operator > (int const,CircularInt const &);
-    friend bool operator > (CircularInt const &,CircularInt const &);
+    friend bool operator > (const CircularInt &,int const);
+    friend bool operator > (int const,const CircularInt  &);
+    friend bool operator > (const CircularInt  &,const CircularInt  &);
     //*= /= += -= //
     CircularInt& operator += (int num);
-    CircularInt& operator += (CircularInt const & r);
+    CircularInt& operator += (const CircularInt  & r);
 
     CircularInt& operator -= (int num);
-    CircularInt& operator -= (CircularInt const & r);
+    CircularInt& operator -= (const CircularInt  & r);
 
     CircularInt& operator *= (int num);
-    CircularInt& operator *= (CircularInt const & r);
+    CircularInt& operator *= (const CircularInt  & r);
 
-    CircularInt& operator /= (CircularInt const & r);
+    CircularInt& operator /= (const CircularInt  & r);
     CircularInt& operator /= (int num );
     //++ -- //
     CircularInt& operator ++ ();
@@ -68,14 +68,13 @@ public:
 
     CircularInt& operator -- ();
     const CircularInt operator -- (int);
-    //
     CircularInt operator - ();
-    friend CircularInt operator - (int num, CircularInt const & r );
-    friend CircularInt operator + (CircularInt const & r, CircularInt const & l);
+    friend CircularInt operator - (int num, const CircularInt & r );
+    friend CircularInt operator + (const CircularInt & r, const CircularInt & l);
 
 
 
-    friend CircularInt& operator /(CircularInt const &r ,int num);
+    friend CircularInt& operator /(const CircularInt &r ,int num);
 
     //Overiding "toString" Object
     friend ostream &operator<<( ostream &output, const CircularInt& t ){
@@ -87,7 +86,7 @@ public:
 inline bool operator<=(const CircularInt & r, int const l) {
     return r.CurrTime <= l;
 }
-inline bool operator<=(int const r , CircularInt const & l ) {
+inline bool operator<=(int const r ,const CircularInt & l ) {
     return r <= l.CurrTime;
 }
 inline bool operator<=(const CircularInt & r, const CircularInt & l ) {
@@ -114,12 +113,12 @@ inline bool operator<(const CircularInt & r,const CircularInt & l ) {
     return r.CurrTime < l.CurrTime;
 }
 
-inline bool operator>(CircularInt const & r, int const l) {
+inline bool operator>(const CircularInt & r, int const l) {
     return r.CurrTime > l;
 }
 inline bool operator>(int const r , CircularInt const & l ) {
     return r > l.CurrTime;
 }
-inline bool operator>(CircularInt const & r, CircularInt const & l ) {
+inline bool operator>(const CircularInt & r, const CircularInt & l ) {
     return r.CurrTime >= l.CurrTime;
 }
